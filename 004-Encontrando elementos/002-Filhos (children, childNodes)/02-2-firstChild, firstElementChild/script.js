7201bf524abbeab008fb41fb4ed56478de29ff71
isaac-santos <isaacconceicao26@gmail.com>
2024-12-18 20:15:00 -0300

    fiz a segunda parte da aula de filhos
    firstChild e firstElementChild

---
+++ 004-Encontrando elementos/002-Filhos (children, childNodes)/02-2-firstChild, firstElementChild/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const container = document.querySelector(".container");
+
+// console.log(container.firstChild); pega o primeiro filho que no caso é o espaço
+console.log(container.firstElementChild);
